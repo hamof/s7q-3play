5afee1f66d7f2f5dd513db605675404e14a26f29
hamof <40973927+hamof@users.noreply.github.com>
2018-09-01 05:49:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -262,5 +262,18 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
     
    }
    }); 
+
+  const Discord = require('discord.js');
+const epic = new Discord.Client();
+
+epic.on('ready',async () => {
+  epic.channels.find(ch => ch.id === "480380218742276096" && ch.type === 'voice').join();
+});
+
+// طبعا عشان الكود يشتغل بكفاءة لازم يكون البوت مفتوح 24 ساعة
+// البوت بيخرج برا الروم اذا الخادم قفل
+// جميع الحقوق محفوظة لسيرفر كودز
+
+epic.login("process.env.BOT_TOKEN")
    
 client.login(process.env.BOT_TOKEN);
